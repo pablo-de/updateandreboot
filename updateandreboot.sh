b7b5420e03c88d8db335cbf62d12d254a734a003
Pablo <94941513+pablo-de@users.noreply.github.com>
2023-02-09 10:37:59 -0300

    change structure in code

--- updateandreboot.sh
+++ updateandreboot.sh
@@ -6,20 +6,29 @@
 BOT_TOKEN="<your bot token>"
 CHAT_ID="<your chat ID>"
 
-MESSAGE_OK="$HOSTNAME: Upgrade reboot realizado con exito."
-MESSAGE_FAIL="$HOSTNAME: Fallo en alguna actualización o reinicio."
+MESSAGE_OK="✅ Raspberry: Upgrade realizado con éxito."
+MESSAGE_FAIL="⛔ Raspberry: Fallo en alguna actualización o reinicio."
+LOG_FILE="$DIR/updates_logs.log"
 
-# Actualizar el sistema
-if sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo pihole -up && sudo apt-get autoremove -y && sudo apt-get clean; then
-  echo "$(date -d 'now' '+%d/%m/%Y %H:%M:%S'): $HOSTNAME: Todas las actualizaciones y reinicio exitosos" >>/usr/bin/tareasemanal/updates_logs.log
+set -e
 
+patching() {
+  sudo apt-get update &&
+  sudo apt-get upgrade -y &&
+  sudo apt-get dist-upgrade -y &&
+  sudo pihole -up &&
+  sudo apt-get autoremove -y &&
+  sudo apt-get clean
+
+  return $?
+}
+
+if patching; then
   curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d parse_mode=Markdown -d text="$MESSAGE_OK" >/dev/null
-
+  echo "$(date '+%d/%m/%Y %H:%M:%S'): $HOSTNAME: Todas las actualizaciones y reinicio exitosos" >> "$LOG_FILE"
   sleep 5s
-
   sudo reboot
 else
-  echo "$(date -d 'now' '+%d/%m/%Y %H:%M:%S'): $HOSTNAME: Fallo en alguna actualización o reinicio" >>/usr/bin/tareasemanal/updates_logs.log
-  sleep 5s
   curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d parse_mode=Markdown -d text="$MESSAGE_FAIL" >/dev/null
+  echo "$(date '+%d/%m/%Y %H:%M:%S'): $HOSTNAME: Fallo en alguna actualización o reinicio" >> "$LOG_FILE"
 fi
